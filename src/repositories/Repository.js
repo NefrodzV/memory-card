d294051037849f73d06a1d05ca0dd3c418858e14
Neftaly <neftalyrv23@gmail.com>
2023-10-30 15:08:55 -0400

    Made return a promise

--- src/repositories/Repository.js
+++ src/repositories/Repository.js
@@ -3,17 +3,15 @@ import WebService from "../WebService"
 export default function Repository() {
     
     const service = WebService()
-    
+   
     const getImages = () => {
         const json = service.getRandomImages()
-        json.then((response) => {
-            console.log(response.data)
-        })
+        return json
     }
 
-    getImages()
+    
 
-
+    
     return {
         getImages
     }
